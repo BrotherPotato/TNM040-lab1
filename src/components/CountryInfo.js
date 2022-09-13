f829ac3d0184fe9bd35d7bdcab8a5e4b86ff4367
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-09-13 16:12:53 +0200

    f

---
+++ src/components/CountryInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const CountryInfo = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.values(country.languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt="flag" width="200px" />
+    </div>
+  );
+}
+//export default ExampleApp;
