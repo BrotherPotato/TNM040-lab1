916937e542c405ccd130a28d56cd411631f6e416
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-09-13 16:09:43 +0200

    woooaa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import countries from 'world-countries'
+import './components/CountryInfo.js';
 
 function App() {
   console.log(countries)
@@ -10,5 +11,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
