bbaf5aea2b77f59ba04c1fccf84fe49d6b3fb49b
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-09-19 14:27:50 +0200

    s

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const m = 15;
 function App() {
   console.log(countries)
 
-  countries.sort((a, b) => {
+  countries = countries.sort((a, b) => {
     return b.area - a.area;
   });
 
-  countries.filter((a) => {
-    return a.name.common != "Antarctica";
+  countries = countries.filter((country) => {
+    return country.name.common !== "Antarctica";
   });
  
   return (
@@ -26,20 +26,13 @@ function App() {
 
       <div className='part2Div'>
         <div className='col1'>
-          {countries.filter(country => { 
-            return country.capital != "";
-            //return country.name.common != "Antarctica";
-          }).slice(0, n).map((c) => (
+          {countries.slice(0, n).map((c) => (
             <CountryInfo data={c} details={true} key={c.ccn3}/>
           ))}
-
         </div>
       
         <div className='col2'>
-          {countries.filter(country => { 
-            return country.capital != "";
-            //return country.name.common != "Antarctica";
-          }).slice(n, m).map((c) => (
+          {countries.slice(n, m).map((c) => (
             <CountryInfo data={c} details={false} key={c.ccn3}/>
           ))}
         </div>
